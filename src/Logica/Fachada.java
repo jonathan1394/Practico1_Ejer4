58cb0655c3314d2d46874b46fd704f1e03227a3f
jonathan1394 <jonhy.jona@gmail.com>
2024-08-13 15:32:03 -0300

    agrego un poco mas de logica y genero el package para los controladores

--- src/Logica/Fachada.java
+++ src/Logica/Fachada.java
@@ -1,7 +1,9 @@
 package Logica;
 import Logica.Mensaje;
+import Persistencia.Persistencia;
 import java.util.List;
 
+import Excepciones.PersistenciaException;
 import ValueObject.VOMensajes;
 
 public class Fachada {
@@ -15,18 +17,31 @@ public class Fachada {
 	
 	public void IngresarMensajes(VOMensajes VOM) {
 		//desde la GUI traemos el texto y el Usuario (Ahora lo hacemos Harcode) lo guardamos en un VO y lo manejamos aca
+		Mensaje m=null;
+		m.setmensaje(VOM.getMensaje());
+		m.setUsuario("Null");
+		M.add(m);
 	}
 	
 	public List<VOMensajes> ListarMensajes(){
 		//Obtenemos la lista la pasamos a VO y se la mandamos a la GUI para mostrar
 		List<VOMensajes> LM=null;
-		
+		for (Mensaje aux : M) {
+			VOMensajes voaux = new VOMensajes(aux.getmensaje(), aux.getUsuario());
+			LM.add(voaux);
+		}
 		return LM;
-		
 	}
 	
-	public void RespaldarMensaje () {
-		
+	public void RespaldarMensaje () throws PersistenciaException {
+		Persistencia p=new Persistencia();
+		List<VOMensajes> LM=null;
+		for (Mensaje aux : M) {
+			VOMensajes voaux = new VOMensajes(aux.getmensaje(), aux.getUsuario());
+			LM.add(voaux);
+		}
+		p.Respaldar(LM);
+
 	}
 	
 	public void RecuperarMensaje () {
--- src/Persistencia/Persistencia.java
+++ src/Persistencia/Persistencia.java
@@ -4,6 +4,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
+
 import ValueObject.VOMensajes;
 import Excepciones.PersistenciaException;
 
@@ -12,12 +14,12 @@ import Excepciones.PersistenciaException;
 public class Persistencia {
 	private String Ruta="./datos.dat";
 	
-	public VOMensajes Recuperar() throws PersistenciaException,  java.lang.ClassNotFoundException {
+	public List<VOMensajes> Recuperar() throws PersistenciaException,  java.lang.ClassNotFoundException {
 		try {
 			FileInputStream fileIn = new FileInputStream(Ruta);
 			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
 	
-			VOMensajes recuperado = (VOMensajes) objectIn.readObject();
+			List<VOMensajes> recuperado = (List<VOMensajes>) objectIn.readObject();
 	
 			objectIn.close();
 			fileIn.close();
@@ -29,13 +31,13 @@ public class Persistencia {
 	
 	
 	
-	public void Respaldar(VOMensajes vo) throws PersistenciaException  {
+	public void Respaldar(List<VOMensajes> Lvo) throws PersistenciaException  {
 		try {
 			FileOutputStream fileOut = new FileOutputStream(Ruta);
 			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
 	
 			// Guarda todos los datos relevantes en el archivo binario
-			objectOut.writeObject(vo);
+			objectOut.writeObject(Lvo);
 			//objectOut.writeObject(asignaturas);
 			// Añade más líneas para guardar otros datos necesarios
 	
